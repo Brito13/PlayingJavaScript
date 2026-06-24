d9655e8ded97ae7340d5c7a41000e1aeb3391148
Raidy <raidi16150905@gmail.com>
2026-06-24 09:43:21 -0400

    First

---
+++ 01-Fundamentals/app.js
@@ -0,0 +1,14 @@
+
+let a = 10, 
+    l = 4, 
+    b = 10, 
+    c = 'Hola ', 
+    d = 'Spiderman',
+    x = a + b; 
+    
+console.log('%c Mis variables', 'color:red; font-weight: bold');
+console.log({c});
+console.log({d});
+console.log('%c My variables', 'color:red; font-weight: bold');
+console.table({a,l,b,c,d});
+const saludo = c + d
