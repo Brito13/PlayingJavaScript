08cd4012acf587048ff7a12cd7ad67d1fde84406
Raidy <raidi16150905@gmail.com>
2026-06-30 07:33:49 -0700

    01-fundamentals

---
+++ 01-Fundamentals/Assets/js/alerts.js
@@ -0,0 +1,17 @@
+
+
+// alert(saludo);
+
+
+
+
+let option = confirm("Desea continuar ?");
+
+if(option){
+    let nombre = prompt('Introduzca el nombre','Nombre' )
+    alert("Bienvenido " + nombre)
+}
+else {
+    alert("Operacion cancelada")
+}
+
---
+++ 01-Fundamentals/Assets/js/primitivos.js
@@ -0,0 +1,22 @@
+let nombre = 'peter parker';
+console.log(nombre);
+
+ nombre = 'ben 10'
+ console.log(nombre);
+
+
+ nombre = "Raidy Brito"
+ console.log(nombre);
+
+ nombre = 123;
+ console.log(nombre)
+
+ console.log(typeof nombre)
+
+ let esAdulto = true;
+ console.log( typeof esAdulto);
+
+ let symbol1 = Symbol('R');
+ let symbol2 = Symbol('R');
+
+ console.log(symbol1 === symbol2);
