4371ac41cd8bca84c59656c0031e9162be3fac2a
Raidy <raidi16150905@gmail.com>
2026-07-17 07:04:23 -0700

    if-else lab done

--- 01-Fundamentals/Assets/js/if-else.js
+++ 01-Fundamentals/Assets/js/if-else.js
@@ -1,4 +1,35 @@
 const hoy = new Date();
-console.log(hoy);
+// console.log(hoy);
+
+let dia = hoy.getDay();
+// console.log(dia);
+
+
+const diaSemana = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado']
+console.table(diaSemana)
+console.log(`Hoy es ${diaSemana[dia]}`);
+
+
+
+
+
+// if (dia = 0) {
+//     console.log('Domingo');
+// } else{
+//     console.log('No es domingo');
+// }
+
+
+// if (dia === '5') {
+//     console.log('hoy es viernes');
+// }else{
+//     console.log('Otro dia culaquiera');
+// }
+
+// if (dia == '5') {
+//     console.log('hoy es viernes');
+// }else{
+//     console.log('Otro dia culaquiera');
+// }
 
 
