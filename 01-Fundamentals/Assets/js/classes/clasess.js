ccdf04fe578b9b9d16e7affe92d8b943c9584ec9
Raidy Brito <raidi16150905@gmail.com>
2026-08-12 10:20:17 -0400

    methods and getters/setters

--- 01-Fundamentals/Assets/js/classes/clasess.js
+++ 01-Fundamentals/Assets/js/classes/clasess.js
@@ -4,16 +4,46 @@ class Persona{
     nombre;
     edad;
     frase;
+    comida;
 
     constructor(nombre,edad,frase){
         this.nombre = nombre;
         this.edad = edad;
         this.frase = frase;
     }
+
+    set setComida(comida){
+        this.comida = comida;
+    }
+
+    get getComida(){
+        return `the favority food of ${this.nombre} is ${this.comida}`
+    }
+
+
+
+    quiensoy(){
+        console.log(`this is ${this.nombre} and my frase is ${this.frase}`);
+    }
+
+    myAgeIs(){
+        console.log(`My name is ${this.nombre} and i am ${this.edad}`);
+    }
+
 }
+
 
 
 const raidy = new Persona('Raidy Brito',23,'La fe existe');
 const fernando = new Persona('Fernando Suero', 28, 'oh oh oh');
+raidy.setComida = 'Espaguetti';
+fernando.comida = 'Sancocho';
+
+console.log(fernando.getComida);
+console.log(raidy.getComida);
+
 console.log(raidy);
 console.log(fernando);
+
+raidy.quiensoy();
+fernando.myAgeIs();
